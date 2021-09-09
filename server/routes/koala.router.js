e7b24fd62ced1acd3c8e2e3147e6b7fb84c2f32c
Danny Mussie <dannymussie7@gmail.com>
2021-09-09 15:41:08 -0500

    added router.get in koala.router

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
@@ -5,7 +6,15 @@ const koalaRouter = express.Router();
 
 
 // GET
-
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "koalas" ORDER BY "name";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting koalas', error);
+        res.sendStatus(500);
+    });
+});
 
 // POST
 
