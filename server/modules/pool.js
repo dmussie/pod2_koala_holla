ca8dd2a2c58e3f9a86f6ad378d65cb7a70a92eb6
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-10 11:48:07 -0500

    Update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,24 +1,24 @@
 const pg = require('pg');
 
 //for //for mac users with homebrew and postico
-// const config = {
-//     database: 'koalas', 
-//     host: 'localhost', 
-//     port: 5432, 
-//     max: 10, 
-//     idleTimeoutMillis: 30000 
-//   };
+const config = {
+    database: 'koalas', 
+    host: 'localhost', 
+    port: 5432, 
+    max: 10, 
+    idleTimeoutMillis: 30000 
+  };
 
 // for windows users with pgAdmin4
-const config = {
-  user: 'postgres', // Add db user
-  host: 'localhost',
-  database: 'koalas', // Add db name
-  password: '1211', // Add db password
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000
-};
+// const config = {
+//   user: 'postgres', // Add db user
+//   host: 'localhost',
+//   database: 'koalas', // Add db name
+//   password: '1211', // Add db password
+//   port: 5432,
+//   max: 10,
+//   idleTimeoutMillis: 30000
+// };
 
   const pool = new pg.Pool(config);
 
