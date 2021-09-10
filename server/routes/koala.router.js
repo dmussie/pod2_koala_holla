a9c50e5f2adb29937ede7d7932d67e17995c1705
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-10 10:26:02 -0500

    created put route in koala.router and removed variable not being used

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,4 +1,3 @@
-const { application } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
@@ -45,6 +44,20 @@ koalaRouter.post('/', (req, res) => {
 });
 
 // PUT
+koalaRouter.put('/:id', (req, res) => {
+    let reqId = req.params.id;
+    console.log('PUT request for id', reqId);
+    let sqlText = 'UPDATE "koalas" SET "ready_to_transfer" = true WHERE "id" = $1;'
+    pool.query(sqlText, [reqId])
+        .then((result) => {
+            console.log('Koala updated');
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        })
+});
 
 
 // DELETE
